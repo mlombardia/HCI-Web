1643c172c5ca577272cebb4702f0781084eeb90f
rodrifera <rodrigonestorfera@gmail.com>
2020-09-26 19:06:09 -0300

    new tempalte

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <v-toolbar>
+<!--      <v-toolbar-side-icon></v-toolbar-side-icon> -->
+      <v-toolbar-title class="grey--text">
+        <span class="font-weight-light">Site</span>
+        <span>Title</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <!-- dropdown menu -->
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+        <!-- <v-btn text slot="activator"> -->
+        <v-btn text v-on="on">
+          <v-icon left>expand_more</v-icon>
+          <span>Menu</span>
+        </v-btn>
+        </template>
+        <v-list>
+          <!-- v-list-tile is changed to v-list-item -->
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-btn text>
+        <span>Sign Out</span>
+        <v-icon right>exit_to_app</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      { icon: 'home', text: 'Home', route: '/'},
+      { icon: 'contacts', text: 'About', route: '/about'},
+    ]
+  }),
+}
+</script>
