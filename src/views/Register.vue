f71b0b0c822eca9c9f8b9bfad5b3190f048928d0
mariano <marperez@itba.edu.ar>
2020-10-19 12:23:50 -0300

    sign up

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is the register page</h1>
+    
+     <div class="register" align="center" >
+
+    <v-card elevation="24" width="600" height="700" color = "#2d4059"  >
+
+      <v-card-title align = "center" >
+        <span class="white--text">Register</span>
+        </v-card-title>
+    
+      <v-col cols="12">
+
+                <v-text-field label="Name" required>
+                </v-text-field>
+
+                <v-text-field label="Surname" required>
+                </v-text-field>
+                
+                <v-text-field label="Age" required>
+                </v-text-field>
+
+                <v-text-field label="Country" required>
+                </v-text-field>
+
+                <v-text-field label="User" required>
+                </v-text-field>
+
+                <v-text-field label="Password" required>
+                </v-text-field>
+
+                <v-text-field label="Confirm password" required>
+                </v-text-field>
+
+                <a href="/login">
+                    <v-btn class="white--text" color="#F06292" elevation="2" rounded  >Cancel</v-btn>
+                </a>
+                <a href="/">
+                    <v-btn class="white--text" color="#F06292" elevation="2" rounded >Register</v-btn>
+                </a>
+
+        </v-col>
+
+    </v-card>
   </div>
 </template>
