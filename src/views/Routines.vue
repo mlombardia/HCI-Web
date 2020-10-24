9366f0ac22de758a6fbc88f890e5c8e27d432bf7
maxi <mlombardia@itba.edu.ar>
2020-10-24 20:02:59 -0300

    ahora levanta primera primera en routines

--- src/views/Routines.vue
+++ src/views/Routines.vue
@@ -226,6 +226,8 @@ LADO DERECHO
         addedExercises: [],
         user: null,
         isPublic: null,
+        id_routine: null,
+        id_cycle: null,
         cant: 0,
         cantExercises: 0,
          links: [
@@ -273,7 +275,7 @@ LADO DERECHO
     }),
     methods: {
       getAllExercises(){
-        ExercisesApi.getExercises(1,1).then(data=>{
+        ExercisesApi.getExercises(this.id_routine, this.id_cycle).then(data=>{
           //window.alert(data.totalCount);
           this.allExercises = data.results;
         });
@@ -338,6 +340,12 @@ LADO DERECHO
       // CategoriesApi.add({"name": "Legs", "detail": "Legs"});
       // CategoriesApi.add({"name": "Middle Body", "detail": "Middle Body"});
       // CategoriesApi.add({"name": "Upper Body", "detail": "Upper Body"});
+      RoutinesApi.getCurrentUserRoutines().then((data) => {
+        this.id_routine = data.results[0].id;
+        RoutinesApi.getCycles(this.id_routine).then((data) => {
+          this.id_cycle = data.results[0].id;
+        });
+      });
       this.getCategories();
       this.getRoutines();
       this.getAllExercises();
