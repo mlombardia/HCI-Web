bf0b675bee7b6143ffb2d7fea1a24a0bc0d76b09
rodrifera <rodrigonestorfera@gmail.com>
2020-10-24 14:52:04 -0300

    asd

--- src/cycles.js
+++ src/cycles.js
@@ -8,6 +8,10 @@ class CyclesApi {
     }
 
     static async getRoutineCycles(id, controller){
-        return await Api.get(`${Api.baseUrl}/routines/${id}/cycles?page=0&size=10&orderBy=id&direction=asc`, true, controller);
+        return await Api.get(`${Api.baseUrl}/routines/${id}/cycles?page=0&size=99999&orderBy=id&direction=asc`, true, controller);
+    }
+
+    static async deleteCycle(RId, CId, controller){
+        return await Api.delete(`${Api.baseUrl}/routines/${RId}/cycles/${CId}`, true, controller);
     }
 }
